c509b360dd67f7914e831d0be1aa31837c26f6f6
slyamciss <slyamciss@gmail.com>
2022-08-15 16:53:54 -0400

    variables

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -4,4 +4,19 @@ variable "sample" {
   default = "Hello world!"
 }
 
+# String Data Type
+variable "sample1" {
+  default = "Hello world!"
+}
 
+## Single quotes are not supported in Terraform
+
+# Number Data Type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data Type
+variable "sample3" {
+  default = true
+}
