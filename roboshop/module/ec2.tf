096e35eaface8786864791c4422ee14a1a58bf21
slyamciss <slyamciss@gmail.com>
2022-08-16 12:56:45 -0400

    cart spot instance setup

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -1,6 +1,16 @@
-resource "aws_instance" "ec2" {
+#resource "aws_instance" "ec2" {
+#  ami                    = "ami-0bb6af715826253bf"
+#  instance_type          = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_app.id]
+#
+#  tags = {
+#    Name = var.COMPONENT
+#  }
+#}
+
+resource "aws_spot_instance_request" "ec2" {
   ami                    = "ami-0bb6af715826253bf"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]
 
   tags = {
