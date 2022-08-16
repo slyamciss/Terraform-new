e58891f9ecf73e11fdc658c85a7dc0cd8dcb77d4
slyamciss <slyamciss@gmail.com>
2022-08-15 23:09:54 -0400

    roboshop project setup

--- example/01-sg-with-ec2/provider.tf
+++ example/01-sg-with-ec2/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "terraform-bucket-88"
-    key = "sample/terraform.tfstate"
+    key    = "sample/terraform.tfstate"
     region = "us-east-1"
   }
 }
