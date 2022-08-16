6678a4d02595c7be7b7e99d2e3dbfced3686c4b2
slyamciss <slyamciss@gmail.com>
2022-08-16 13:13:18 -0400

    cart spot instance tag setup

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -12,9 +12,15 @@ resource "aws_spot_instance_request" "ec2" {
   ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]
+  wait_for_fulfillment   = true
 
   tags = {
     Name = var.COMPONENT
   }
 }
 
+resource "aws_ec2_tag" "ec2-name-tag" {
+  resource_id = aws_spot_instance_request.ec2.spot_instance_id
+  key         = "Name"
+  value       = var.COMPONENT
+}
